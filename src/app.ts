48b259f1be7cdf8a253671c42bf3944195f751a9
eilonmor <eilonmor@gmail.com>
2023-01-29 13:16:08 +0200

    test

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,36 @@
 import express from 'express';
-
+import {Request, Response} from 'express'
+import bodyParser from 'body-parser';
+// import {  } from 'querystring';
 const app = express();
-
 // TODO implement me :)
+const port = 3003
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
+
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello world!')
+});
+
+app.post('/bmi', (req: Request, res: Response) => {
+    const weight: string = req.query.weight as string;
+    const height:string = req.query.higth as string;
+    const weightUnit : string = req.query.weightUnit  as string;
+    const heightUnit  : string = req.query.heightUnit   as string;    
+    console.log(weight, height, weightUnit, heightUnit);
+
+});
+
+app.post('/bmi', (req: Request, res: Response) => {
+    const {weight, height, weightUnit, heightUnit} = req.body
+    res.send({weight, height, weightUnit, heightUnit})
+
+});
+
+app.listen(port, ()=>{console.log(`server is rannning at http://localhost:${port}`)});
 
 export default app;
+
+
--- src/validator.ts
+++ src/validator.ts
@@ -1,4 +1,5 @@
 import type { NextFunction, Request, Response } from 'express';
+import { send } from 'process';
 
 
 /**
@@ -9,5 +10,25 @@ import type { NextFunction, Request, Response } from 'express';
  */
 export default function validator(req: Request, res: Response, next: NextFunction) {
     // TODO implement me
-    next();
+    const local = `http://localhost:${3002}`
+    const weight: string = req.query.weight as string;
+    const height:string = req.query.higth as string;
+    const weightUnit : string = req.query.weightUnit  as string;
+    const heightUnit  : string = req.query.heightUnit   as string;    
+    
+    function castNumber(str:string):boolean{
+        if (Number.isNaN(Number(str))){
+            return false
+        }else{return true}   
+    }
+    if (castNumber(weight) === false || castNumber(height) === false || castNumber(weightUnit) === false || castNumber(heightUnit) === false){
+        res.status(400).send(`you write a non valide Eror`)
+    }
+    next()
+
+    
+
+
+
+
 }
